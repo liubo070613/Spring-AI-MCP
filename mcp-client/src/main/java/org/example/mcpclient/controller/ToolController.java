f54a83e80072d8f9f1ccc4a8cf9c19ce49be5597
刘波 <liubo070613@163.com>
2025-04-11 10:52:56 +0800

    refactor(mcp-client): 优化 ToolController 并添加注释 - 在 ToolController 类中添加注释，解释如何使用

--- mcp-client/src/main/java/org/example/mcpclient/controller/ToolController.java
+++ mcp-client/src/main/java/org/example/mcpclient/controller/ToolController.java
@@ -10,28 +10,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/tools")
 public class ToolController {
-    private final ChatClient chatClient ;
+    private final ChatClient chatClient;
+
+    /*
+    你引入的依赖（如 spring-ai-alibaba-starter）会自动：
+    根据 spring.ai.alibaba.* 配置创建一个 AlibabaChatClient
+    也会自动注册一个 ChatClient.Builder Bean，可以根据这个Builder 创建一个 自定义的绑定了工具的ChatClient
+    它内部已经帮你设好模型名、API key、URL 等参数
+     */
     public ToolController(ChatClient.Builder aiClientBuilder, ToolCallbackProvider mcpTools) {
         this.chatClient = aiClientBuilder
                 .defaultTools(mcpTools)
-                .build() ;
+                .build();
     }
 
     @GetMapping("/weather")
     public ResponseEntity<String> getWeather(String prompt) {
-        System.err.println(prompt) ;
+        System.err.println(prompt);
         String response = this.chatClient
                 .prompt(prompt)
-                .call().content() ;
-        return ResponseEntity.ok(response) ;
+                .call().content();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/ip")
     public ResponseEntity<String> getIpAddress(String prompt) {
-        System.err.println(prompt) ;
+        System.err.println(prompt);
         String response = this.chatClient
                 .prompt(prompt)
-                .call().content() ;
-        return ResponseEntity.ok(response) ;
+                .call().content();
+        return ResponseEntity.ok(response);
     }
 }
